9dc24c2c48090f4637a2b24d9f83bee4efdfe2f5
AlekseyShaykin <alekseyshaykin2311@gmail.com>
2022-10-30 11:46:36 +0700

    Task #54 has done

--- Seminar_8_Task_54/Program.cs
+++ Seminar_8_Task_54/Program.cs
@@ -10,8 +10,9 @@
 
 int[,] matrix =
 {
-    {5,1,9,4},
-    {5,1,9,4},
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4},
 };
 
 
@@ -40,12 +41,13 @@ void Method(int[,] matr)
             int max = j;
             for (int k = j + 1; k < matr.GetLength(1); k++)
             {
-                if (matr[i, k] < matr[i, j]) min = k;
+                if (matr[i, k] > matr[i, max]) max = k;
             }
-            int temp = matr[i,j];
-            matr[i,j] = matr[i,min];
-            matr[i,min] = temp;
-
+            
+            int temp = matr[i, j];
+            matr[i, j] = matr[i, max];
+            matr[i, max] = temp;
+            
         }
     }
 }
@@ -53,23 +55,3 @@ void Method(int[,] matr)
 
 Method(matrix);
 PrintMatrix(matrix);
-
-
-
-
-
-// void SelectionSort(int[] array)
-// {
-//     for (int i = 0; i < array.Length - 1; i++)
-//     {
-//         int min = i;
-//         for (int j = i + 1; j < array.Length; j++)
-//         {
-//             if (array[j] < array[min]) min = j;
-//         }
-
-//         int temp = array[i];
-//         array[i] = array[min];
-//         array[min] = temp;
-//     }
-// }
